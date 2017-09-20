b14379279a133437731bfbdce9622ef1101be7bf
Muhammad Khadafi <muhammadkhadafi@Muhammads-MBP.fios-router.home>
2017-09-20 02:00:22 -0400

    jessica

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/power" => "tv#power"
   match "/james", to: "tv#james", via: [:get, :put]
+  match "/jessica", to: "tv#jessica", via: [:get, :put]
 end
